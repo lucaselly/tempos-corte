cb7d609944c66ed4a22266387ed821e563be77ae
Lucas Elly <lucasalvesferraz@gmail.com>
2022-09-11 17:21:35 -0300

    Base

--- temposCorte.py
+++ temposCorte.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
+# Criar um DataFrame de um .CSV
 temposCorteDados = pd.read_csv("temposCorte-data.csv")
 
-temposCorteDados = pd.DataFrame(temposCorteDados)
-
+# Separar apenas as colunas úteis
 temposCorte = temposCorteDados[[
     'Tipo_de_corte', 'Modelo', 'Tempo_s', 'Categoria']]
 
+# Criando uma cópia para transformar a coluna Tempo_s em Float
 temposCortes = temposCorte.copy()
 temposCortes.Tempo_s = temposCorte.Tempo_s.str.replace(',', '').astype(float)
+
+tipoCorte = {'CABEDAL': [], 'FORRO': [], 'COMPLETO': []}
+
+categoria = {'Bota': [], 'Coturno': [], 'Flat': [], 'Flatform': [], 'Loafer': [], 'Mule': [
+], 'Open': [], 'Oxford': [], 'Rasteira': [], 'Sapatilha': [], 'Slipper': [], 'Sneaker': []}
+
+final = tipoCorte.copy()
+for tipo in tipoCorte:
+    final[tipo] = categoria
+print(final)
